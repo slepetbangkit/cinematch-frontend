e696ee4dfa8e93f179a10972e7e8ba47e32a1d52
Muhammad Iqbal Dwitama <muhammad.iqbal111@ui.ac.id>
2024-06-07 17:57:41 +0700

    feat: onboarding welcome activity

--- app/src/main/java/com/slepetbangkit/cinematch/view/main/MainActivity.kt
+++ app/src/main/java/com/slepetbangkit/cinematch/view/main/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.slepetbangkit.cinematch.view.main
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -9,6 +12,9 @@ import com.slepetbangkit.cinematch.R
 import com.slepetbangkit.cinematch.data.local.preferences.SessionPreferences
 import com.slepetbangkit.cinematch.data.local.preferences.dataStore
 import com.slepetbangkit.cinematch.databinding.ActivityMainBinding
+import com.slepetbangkit.cinematch.view.welcome.WelcomeActivity
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -20,9 +26,29 @@ class MainActivity : AppCompatActivity() {
         sessionPreferences = SessionPreferences.getInstance(this.dataStore)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
 
+        lifecycleScope.launch {
+            val accessTokenFlow = sessionPreferences.getAccessToken()
+            val refreshTokenFlow = sessionPreferences.getRefreshToken()
+
+            combine(accessTokenFlow, refreshTokenFlow) { accessToken, refreshToken ->
+                Pair(accessToken, refreshToken)
+            }.collect { (accessToken, refreshToken) ->
+                if (accessToken.isBlank() || refreshToken.isBlank()) {
+                    val intent = Intent(this@MainActivity, WelcomeActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    setupNavBar()
+                }
+            }
+        }
+
+    }
+    private fun setupNavBar() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
         val bottomNavigationView: BottomNavigationView = binding.bottomNavigation
         bottomNavigationView.setupWithNavController(navController)
     }
